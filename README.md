22396663a9104c5f1fde0e213254aed345dc5c6f
lol-chang <doclecsj@naver.com>
2023-11-18 15:10:44 +0900

    Squashed commit of the following:
    commit 8f78a65839b189e75a0199464caf2efe8d886d05
    Author: foxirain <ha2955@naver.com>
    Date:   Sat Nov 18 15:06:27 2023 +0900
    
        1차

--- README.md
+++
@@ -1,2 +0,0 @@
-# 2023_News_BigData_Hackathon
-2023 뉴스 빅데이터 해커톤 공모전 
---
+++ main.py
@@ -0,0 +1,11 @@
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="template")
+
+@app.get("/")
+async def read_root(request: Request):
+    return templates.TemplateResponse("article2.html", {"request": request}) 
