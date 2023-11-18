ddd3e0392283f066e28b8631dd30bea8fcbd3c08
lol-chang <doclecsj@naver.com>
2023-11-18 19:33:14 +0900

    중간 저장...
    ㅜㅜ

--- bigkinds.py
+++ bigkinds.py
@@ -1,7 +1,32 @@
 import requests
 import json
-
 import re
+
+
+import openai
+openai.api_key = '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'
+# 모델을 변경을 해야 할지? 
+model = "text-davinci-003"
+# model= "gpt-3.5-turbo"
+def gpt(query):
+  # openai migrate 업데이트 필요함 
+  response = openai.Completion.create(
+    engine=model,
+    prompt=query,
+    max_tokens=550 #글자수 제한
+  )
+  answer = response.choices[0].text.strip()
+  return answer
+
+def ordinance_from_gpt(x):
+    user = x + "이 내용을 보고 독특한 조례를 3가지를 만들어서 리스트 형식으로 리턴해주세요."
+    key_word = gpt(user)
+    print(key_word)
+
+def connect_from_gpt(x):
+    user = x + "이 키워드를 보고 관련 있는 산업 분야 5가지를 리스트 형식으로 리턴해주세요. "
+    new = gpt(user)
+    print(new)
 
 def remove_html_tags(text):
     clean = re.compile('<.*?>')
@@ -13,10 +38,10 @@ def search_news():
     data = {           
         "access_key": "d66bd288-5a22-43d1-8db1-37fbfbd37b7f",
         "argument": {
-            "query": "서비스 AND 출시",
+            "query": "",
             "published_at": {
                 "from": "2019-01-01",
-                "until": "2019-03-31"
+                "until": "2023-03-31"
             },
             "provider": ["경향신문"],
             "category": ["정치>정치일반", "IT_과학"],
@@ -31,7 +56,7 @@ def search_news():
             "sort": {"date": "desc"},
             "hilight": 200,
             "return_from": 0,
-            "return_size": 5,
+            "return_size": 1,
             "fields": [
                 "byline",
                 "category",
@@ -46,8 +71,58 @@ def search_news():
     if respone.status_code == 200:
         parsed_data = respone.json()
         for i in parsed_data['return_object']['documents']:
-            x = remove_html_tags(i['hilight'])
-            res.append([i['provider'], i['title'], x])
+            hilight = remove_html_tags(i['hilight'])
+            keyword = create_keyword(i['title'], hilight)
+            res.append([i['provider'], i['title'], hilight, keyword])
         return res
     else:
         print("API 호출 실패", respone.status_code)
+
+#hilight 내용을 보고 키워드를 추출는 FUNC
+def create_keyword(title, hilight):
+    url = "https://tools.kinds.or.kr/keyword"
+    data = { 
+        "access_key": "d66bd288-5a22-43d1-8db1-37fbfbd37b7f",
+            "argument": {
+                "title": title, 
+                "sub_title": "", 
+                "content": hilight
+            }
+        }
+    respone = requests.post(url, json=data)
+    res = []
+    if respone.status_code == 200:
+        parsed_data = respone.json()
+        print(list(parsed_data['return_object']['result']['content'].split()))
+        # connect_from_gpt(parsed_data)
+        # print("keyword: ", parsed_data)
+    else:
+        print("API 호출 실패", respone.status_code)
+    
+
+def news_id_search(): #이 함수는 아이디 가지고 기사 분석 가능
+    url = "https://tools.kinds.or.kr/search/news"
+
+    data = {
+        "access_key": "d66bd288-5a22-43d1-8db1-37fbfbd37b7f", 
+        "argument": {
+            "news_ids": [ "01500701.2015083110018412570", "01100701.20150826100000152"],
+            "fields": [
+                "content",
+                "byline",
+                "category",
+                "category_incident",
+                "images",
+                "provider_subject",
+                "provider_news_id",
+                "publisher_code"] 
+        }
+    }
+
+    respone = requests.post(url, json=data)
+    if respone.status_code == 200:
+        parsed_data = respone.json()
+        return parsed_data
+    else:
+        print("API 호출 실패", respone.status_code)
+
--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-
-# from fastapi import FastAPI, Request
-# from fastapi.responses import HTMLResponse
-# from fastapi.templating import Jinja2Templates
-
-# app = FastAPI()
-# templates = Jinja2Templates(directory="template")
-
-# @app.get("/")
-# async def read_root(request: Request):
-#     return templates.TemplateResponse("article2.html", {"request": request})
-
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -19,8 +7,14 @@ app = FastAPI()
 templates = Jinja2Templates(directory="template")
 
 data2 = bigkinds.search_news()
+data3 = bigkinds.news_id_search()
 
 @app.get("/")
 async def read_root(request: Request):
     return templates.TemplateResponse("article2.html", {"request": request, "data": data2})
 
+@app.get("/data3")
+async def read_root(request: Request):
+    return templates.TemplateResponse("x.html", {"request": request, "data": data3})
+    ## return 값을 html 에서 사용하려면 
+    # {{ data.return_object.total_hits }} data.return_object사용
