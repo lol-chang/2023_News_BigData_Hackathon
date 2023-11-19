08f3daf3af68bf02c142e134da7283ab2cc15db6
lol-chang <doclecsj@naver.com>
2023-11-19 23:31:01 +0900

    backup - 90%
    code change

--- bigkinds.py
+++ bigkinds.py
@@ -1,15 +1,16 @@
 import requests
-import json
 import re
 
-
 import openai
-openai.api_key = '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'
-# 모델을 변경을 해야 할지? 
+
+
+openai.api_key = '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'
 model = "text-davinci-003"
 # model= "gpt-3.5-turbo"
+
 def gpt(query):
   # openai migrate 업데이트 필요함 
+
   response = openai.Completion.create(
     engine=model,
     prompt=query,
@@ -21,12 +22,12 @@ def gpt(query):
 def ordinance_from_gpt(x):
     user = x + "이 내용을 보고 독특한 조례를 3가지를 만들어서 리스트 형식으로 리턴해주세요."
     key_word = gpt(user)
-    print(key_word)
+    return key_word
 
 def connect_from_gpt(x):
     user = x + "이 키워드를 보고 관련 있는 산업 분야 5가지를 리스트 형식으로 리턴해주세요. "
     new = gpt(user)
-    print(new)
+    return new
 
 def remove_html_tags(text):
     clean = re.compile('<.*?>')
@@ -70,10 +71,19 @@ def search_news():
     res = []
     if respone.status_code == 200:
         parsed_data = respone.json()
-        for i in parsed_data['return_object']['documents']:
-            hilight = remove_html_tags(i['hilight'])
-            keyword = create_keyword(i['title'], hilight)
-            res.append([i['provider'], i['title'], hilight, keyword])
+
+        news_id = parsed_data['return_object']['documents'][0]['news_id']
+        print(news_id_search([news_id]))
+
+        title = parsed_data['return_object']['documents'][0]['title']
+        hilight = remove_html_tags(parsed_data['return_object']['documents'][0]['hilight'])
+        provider = parsed_data['return_object']['documents'][0]['provider']
+        keyword = create_keyword(parsed_data['return_object']['documents'][0]['title'], hilight)
+        related_industry = related_industries(keyword, hilight)
+
+        #AI 조례 생성
+        # created_ordinance = ordinance_from_gpt()
+        res.append([provider, title, hilight, related_industry])
         return res
     else:
         print("API 호출 실패", respone.status_code)
@@ -93,20 +103,18 @@ def create_keyword(title, hilight):
     res = []
     if respone.status_code == 200:
         parsed_data = respone.json()
-        print(list(parsed_data['return_object']['result']['content'].split()))
-        # connect_from_gpt(parsed_data)
-        # print("keyword: ", parsed_data)
+        return list(set(parsed_data['return_object']['result']['content'].split()))
     else:
         print("API 호출 실패", respone.status_code)
     
 
-def news_id_search(): #이 함수는 아이디 가지고 기사 분석 가능
+def news_id_search(id): #이 함수는 아이디 가지고 기사 분석 가능
     url = "https://tools.kinds.or.kr/search/news"
 
     data = {
         "access_key": "d66bd288-5a22-43d1-8db1-37fbfbd37b7f", 
         "argument": {
-            "news_ids": [ "01500701.2015083110018412570", "01100701.20150826100000152"],
+            "news_ids": id,#[ "01500701.2015083110018412570", "01100701.20150826100000152"],
             "fields": [
                 "content",
                 "byline",
@@ -126,3 +134,11 @@ def news_id_search(): #이 함수는 아이디 가지고 기사 분석 가능
     else:
         print("API 호출 실패", respone.status_code)
 
+
+def related_industries(keyword, hilight):
+    user = "키워드: "+str(keyword) + ", 내용: " + str(hilight) + "이야. 이 키워드와 내용을 분석해서 이 기사가 영향을 주는 산업 분야를 5가지로 파이썬 리스트를 만들어서 리스트만 리턴해줘"
+    related = gpt(user)
+    return related
+
+# ordinance_from_gpt()
+print(search_news())
--- main.py
+++ main.py
@@ -7,12 +7,20 @@ app = FastAPI()
 templates = Jinja2Templates(directory="template")
 
 data2 = bigkinds.search_news()
-
 data3 = bigkinds.news_id_search()
+#data4 = bigkinds.search_test()
+#print(data4)
+
+# 회원가입 정보 KEY값 =============
+user_age = 20;
+user_job ="학생";
+user_gender='M';    
+user_country="서울";
+# ================================
 
 @app.get("/")
-async def read_root(request: Request):
-    return templates.TemplateResponse("article2.html", {"request": request, "data": data2})
+async def read_root(request: Request):  
+    return templates.TemplateResponse("article2.html", {"request": request, "data": data2 , "age": user_age,"job": user_job, "gender": user_gender , "country": user_country})
     
 
 @app.get("/data3")
@@ -20,8 +28,36 @@ async def read_root(request: Request):
     return templates.TemplateResponse("x.html", {"request": request, "data": data3})
     ## return 값을 html 에서 사용하려면 
     # {{ data.return_object.total_hits }} data.return_object사용
-
+@app.get("/search")
+async def show_login(request: Request):
+    return templates.TemplateResponse("search.html", {"request": request})
 @app.get("/login")
 async def show_login(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
+@app.get("/plaza")
+async def show_login(request: Request):
+    return templates.TemplateResponse("plaza.html", {"request": request})
 
+@app.get("/debate")
+async def show_login(request: Request):
+    return templates.TemplateResponse("debate.html", {"request": request})
+
+
+@app.get("/user_info")
+async def read_root(request: Request):  
+    return templates.TemplateResponse("user_info.html", {"request": request, "age": user_age,"job": user_job, "gender": user_gender , "country": user_country})
+
+@app.get("/get_data")
+async def get_data():
+    # 여기에서 데이터를 가져오는 로직을 구현하고 데이터를 반환합니다.
+    # 이 부분은 실제 데이터를 가져오는 로직으로 대체되어야 합니다.
+    x = []
+    x.append( ['zzz','aaa','18'])
+    data = {"content": x}  # 예시 데이터
+    return data
+
+
+
+# @app.post("/login")
+# async def show_login(request: Request, username: str = Form(…), password: str = Form(…)):
+#     return templates.TemplateResponse("article.html", {"request": request, "username": username, "password": password})
